b4a5e310b0f6645be09e665bbfc85eb691cedfb3
bitesizing <r.raimundo2001@gmail.com>
2024-06-09 17:26:53 +0100

    . minor messing around

--- archive/test.py
+++ archive/test.py
@@ -5,4 +5,10 @@ base_leetcode_url: str = "https://leetcode.com/problems/"
 graphql_url: str = "https://leetcode.com/graphql"
 problemset_query_filename: str = "templates/get-problemset.graphql"
 
-make_post_request_from_query(graphql_url, problemset_query_filename, json_variables={"categorySlug": "", "skip": 0, "limit": 50, "filters": {}})
+a = make_post_request_from_query(graphql_url, problemset_query_filename, json_variables={"categorySlug": "", "skip": 0, "limit": -1, "filters": {}})['data']['problemsetQuestionList']
+
+
+# Get all values for one entry in a list of dicationaries
+dict_values = set()
+for question in a['questions']:
+    dict_values.add(question['paidOnly'])
